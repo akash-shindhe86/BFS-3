c54730440c5e7f12a08107964282a71933f838ab
ashin19 <ashin19@safeway.com>
2024-10-18 09:46:20 -0700

    done

---
+++ Problem2.java
@@ -0,0 +1,32 @@
+// Time Complexity : O(n) 
+// Space Complexity : O(e)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : used the same logic Jaspinder explained in the class.
+
+package BFS-3;
+
+public class Problem2 {
+    public Node cloneGraph(Node node) {
+        if(node == null) return node;
+
+        HashMap<Node,Node> map = new HashMap<>(); // O(v) 
+        Queue<Node> q = new LinkedList<>();
+        //create deep copy
+        Node copy = new Node(node.val);
+        q.add(node);
+        map.put(node,copy);
+
+        while(!q.isEmpty()){
+            Node cur = q.poll();
+            for(Node n: cur.neighbors){ //O(v+e)
+                if(!map.containsKey(n)){
+                    Node copyChild = new Node(n.val);
+                    map.put(n, copyChild);
+                    q.add(n);
+                }
+                map.get(cur).neighbors.add(map.get(n));
+            }
+        }
+        return map.get(node);
+    }
+}
